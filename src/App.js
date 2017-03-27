6d22ba5eb2098fd60d709e4de050d5815979b917
shota-suzuki <shota-suzuki@livesense.co.jp>
2017-03-27 16:11:01 +0900

    add

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import humps from "humps"
 
 class App extends Component {
   render() {
+    const debugData = {
+      "aaaBbb": "cccDdd"
+    }
     return (
       <div className="App">
         <div className="App-header">
@@ -13,6 +17,9 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <pre>{JSON.stringify(debugData)}</pre>
+        <pre>{JSON.stringify(humps.decamelizeKeys(debugData))}</pre>
+
       </div>
     );
   }
